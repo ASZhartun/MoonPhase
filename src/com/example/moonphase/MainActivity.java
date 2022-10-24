a2ffc1b3e99ca7bc49ce9c8847ddc57c67ee19ca
ASZhartun <as.zhartun@gmail.com>
2022-10-24 09:57:12 +0300

    feature: add setting default date values by zone

--- src/com/example/moonphase/MainActivity.java
+++ src/com/example/moonphase/MainActivity.java
@@ -1,34 +1,44 @@
 package com.example.moonphase;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	
+	final String DATE_PATTERN = "dd.MM.yyyy HH:mm:ss";
+
+	EditText dateField;
+	EditText zoneField;
+	
+	ListView moonParams;
+	ListView moonPhases;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		dateField = (EditText) findViewById(R.id.dateField);
+		zoneField = (EditText) findViewById(R.id.zoneField);
+		moonParams = (ListView) findViewById(R.id.moonParams);
+		moonPhases = (ListView) findViewById(R.id.moonPhases);
+		
+		Date curr = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
+		sdf.setTimeZone(TimeZone.getTimeZone("Europe/Minsk"));
+		dateField.setText(sdf.format(curr));
+		int hoursGMT = TimeZone.getTimeZone("Europe/Minsk").getRawOffset() / 3600000;
+		zoneField.setText("GMT: " + Integer.toString(hoursGMT));
+		
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
